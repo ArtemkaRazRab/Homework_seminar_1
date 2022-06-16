0e52cce6dc34ab633b6358bd5aedc5e766ac6907
«Artemka» <sergeev_artem91@mail.ru>
2022-06-16 22:30:25 +0500

    Homework seminar 1

---
+++ homework.java
@@ -0,0 +1,15 @@
+public class homework {
+
+    public static void main (String[] args)
+    {
+        int i,n=9,j; 
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=i;j++)  { 
+            //for(j=n;j>=i;j--) {
+                System.out.print(i);
+            }      
+            System.out.print("\n");        
+        }
+    }
+}
