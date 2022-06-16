292bf864a9f9bc1e894dbcea13fef1d9d3c716e4
«Artemka» <sergeev_artem91@mail.ru>
2022-06-16 23:55:01 +0500

    Homework seminar 1

--- homework.java
+++ homework.java
@@ -1,15 +1,13 @@
-public class homework {
+import java.util.Scanner;
 
-    public static void main (String[] args)
-    {
-        int i,n=9,j; 
-        for(i=1;i<=n;i++)
-        {
-            for(j=1;j<=i;j++)  { 
-            //for(j=n;j>=i;j--) {
-                System.out.print(i);
-            }      
-            System.out.print("\n");        
-        }
+public class homework {
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите число: ");
+        Scanner n = new Scanner(System.in);
+        n.hasNextInt();
+        int i = n.nextInt();
+        System.out.println(Math.abs(0.5 * i * (i + 1)));
+        iScanner.close();
     }
 }
